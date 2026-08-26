1083f4a92003fdd260d8a9ce389e1cfbd2f9fdc8
nakada toyohisa <nakada.toyohisa@gmail.com>
2026-08-26 13:32:20 +0900

    for debug

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -58,6 +58,7 @@ export async function updateSession(request: NextRequest) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
+    console.log("-----------------",url)
     return NextResponse.redirect(url);
   }
 
